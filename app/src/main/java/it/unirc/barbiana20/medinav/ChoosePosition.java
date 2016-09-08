74145e5564016f4731516ab6ab7c2cdd9743b990
Dimitri <schiavone.dimitri.sd@gmail.com>
2016-09-08 22:28:10 +0200

    some code readibility improvements

--- app/src/main/java/it/unirc/barbiana20/medinav/ChoosePosition.java
+++ app/src/main/java/it/unirc/barbiana20/medinav/ChoosePosition.java
@@ -1,10 +1,16 @@
 package it.unirc.barbiana20.medinav;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class ChoosePosition extends CommonActivity {
+//implementing OnItemSelectedListener interface to react to user navigation selection
+public class ChoosePosition extends CommonActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +18,35 @@ public class ChoosePosition extends CommonActivity {
         setContentView(R.layout.activity_choose_position);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.faculty_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+       // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        String selected = parent.getItemAtPosition(pos).toString();
+        AlertDialog.Builder dlgBuilder = new AlertDialog.Builder(this);
+        dlgBuilder.setTitle(R.string.change_floor_dialog_title);
+        dlgBuilder.setMessage("Facoltà scelta: " + selected);
+        dlgBuilder.setCancelable(false);
+        dlgBuilder.setPositiveButton(R.string.ok,new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int id) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alertDialog = dlgBuilder.create();
+        alertDialog.show();
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // // TODO: 08/09/2016 respond to empty choiche
     }
 
     protected void Explore(View view){
