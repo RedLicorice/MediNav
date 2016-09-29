809f4c42bfe694a1b981d86aea433886a225c677
Dimitri <schiavone.dimitri.sd@gmail.com>
2016-09-29 11:57:15 +0200

    overflow menu added

---
+++ app/src/main/java/it/unirc/barbiana20/medinav/About.java
@@ -0,0 +1,18 @@
+package it.unirc.barbiana20.medinav;
+
+import android.os.Bundle;
+
+/**
+ * Created by schia on 28/09/2016.
+ */
+
+public class About extends CommonActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        initToolbar();
+    }
+
+
+
+}
